26fc94646a5e0cc3c4fb3a082a535fbbfaccc548
master-watts <watts.trevor1@gmail.com>
2017-11-28 21:30:54 -0800

    Adding react bootstrap files

---
+++ react class/CoreUI-React/React_Starter/src/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Buttons from './views/Components/Buttons';
+
+class Home extends React.Component{
+	render(){
+		return(
+			<div>
+			<Buttons />
+			</div>
+			)
+	}
+}
+
+export default Home 
---
+++ react class/react-events-walkthough-master/src/Todo.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+
+
+class Todo extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      complete: false
+    }
+  }
+
+  handleClick(e){
+    e.preventDefault();
+    this.setState({
+      complete: true
+    })
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('a');
+    return true
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log('b')
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log('c')
+  }
+
+  componentWillReceiveProps(nextProps){
+    console.log('d')
+    if(nextProps !== this.props){
+      this.setState({
+        complete: false
+      })
+    }
+  }
+
+  componentWillMount(){
+    console.log('e')
+  }
+
+  componentDidMount(){
+    console.log('f')
+  }
+
+
+
+render(){
+  return(
+    !this.state.complete ?
+    <div>
+      <li>{this.props.task}
+
+        <button onClick={ (event)=> this.handleClick(event) }>complete</button>
+      </li>
+      <br></br>
+      mission not complete
+    </div>
+    :
+    <div>
+      <li>{this.props.task}
+      </li>
+      <br></br>
+      mission complete
+    </div>
+    )
+  }
+}
+
+
+
+export default Todo;
